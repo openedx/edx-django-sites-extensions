a63b3203c9c7be299741a3416658c53de9a36e2a
Douglas Hall <douglashall@dhcp-18-189-88-51.dyn.MIT.EDU>
2016-08-31 18:04:34 -0400

    Add a timeout to the in-memory Site model cache

--- django_sites_extensions/tests/test_models.py
+++ django_sites_extensions/tests/test_models.py
@@ -1,9 +1,13 @@
 """ Tests for Django Sites framework models overrides """
+import datetime
+
 from django.contrib.sites.models import Site
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpRequest
 from django.test.testcases import TestCase
 from django.test.utils import override_settings
+
+from django_sites_extensions.models import SITE_CACHE_TIMEOUTS
 
 
 @override_settings(SITE_ID=1)
@@ -15,6 +19,57 @@ class PatchedSiteManagerTestCase(TestCase):
     def setUp(self):
         super(PatchedSiteManagerTestCase, self).setUp()
         self.foo_site = Site.objects.create(domain='foo.com')
+
+    def test_site_cache_timeout_when_none(self):
+        """
+        Test site cache when SITE_CACHE_TIMEOUT is None for the given site.
+        """
+        request = HttpRequest()
+        request.META['HTTP_HOST'] = self.foo_site.domain
+
+        # Test getting current site by request host
+        site = Site.objects.get_current(request)
+        with self.assertNumQueries(0):
+            site = Site.objects.get_current(request)
+
+        del SITE_CACHE_TIMEOUTS[site.domain]
+        with self.assertNumQueries(1):
+            site = Site.objects.get_current(request)
+
+        # Test getting current site by default site ID
+        site = Site.objects.get_current()
+        with self.assertNumQueries(0):
+            site = Site.objects.get_current()
+
+        del SITE_CACHE_TIMEOUTS[site.id]
+        with self.assertNumQueries(1):
+            site = Site.objects.get_current()
+
+    def test_site_cache_timeout_when_expired(self):
+        """
+        Test site cache when SITE_CACHE_TIMEOUT is expired for the given site.
+        """
+        request = HttpRequest()
+        request.META['HTTP_HOST'] = self.foo_site.domain
+        past = datetime.datetime.utcnow() - datetime.timedelta(0, 300)
+
+        # Test getting current site by request host
+        site = Site.objects.get_current(request)
+        with self.assertNumQueries(0):
+            site = Site.objects.get_current(request)
+
+        SITE_CACHE_TIMEOUTS[site.domain] = past
+        with self.assertNumQueries(1):
+            site = Site.objects.get_current(request)
+
+        # Test getting current site by default site ID
+        site = Site.objects.get_current()
+        with self.assertNumQueries(0):
+            site = Site.objects.get_current()
+
+        SITE_CACHE_TIMEOUTS[site.id] = past
+        with self.assertNumQueries(1):
+            site = Site.objects.get_current()
 
     def test_current_site_from_request_host(self):
         """
