c7ad36a8a7cb4724f79cf5d88971ca813c3e3412
Clinton Blackburn <cblackburn@edx.org>
2017-04-19 11:26:51 -0400

    Added conditional around redirect signal
    We only setup the signal for the Redirect model if the requisite app is
    installed. This ensures we avoid errors at startup for projects that do
    not have the app installed.

--- django_sites_extensions/apps.py
+++ django_sites_extensions/apps.py
@@ -8,6 +8,7 @@ class DjangoSitesExtensionsConfig(AppConfig):
     name = 'django_sites_extensions'
     verbose_name = 'Django Sites Extensions'
 
+    # noinspection PyUnresolvedReferences
     def ready(self):
         """ Set up for django_sites_extensions app """
         # pylint: disable=unused-variable
