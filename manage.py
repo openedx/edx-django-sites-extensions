a768687e716e16f6c855f318eb96abeec7e4334e
aarif <mraarif@outlook.com>
2019-11-05 16:11:38 +0500

    Updated the config to upgrade/support django2.2
    updated the django2.2 specific for python3.5
    
    updated the pylint command
    
    changes made to fix/suppress pylint warnings
    
    changes made as suggested
    
    updated the travis config to remove unsupported django environments
    
    updated the version
    
    changes made as suggested

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_sites_extensions.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test_settings")
 
     from django.core.management import execute_from_command_line
 
