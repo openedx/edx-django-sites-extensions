44898496bd73371126a1e7b8181096dcca2fe3af
Aarif <mraarif@outlook.com>
2020-04-21 17:27:32 +0500

    updated the tox and travis configs

--- setup.py
+++ setup.py
@@ -12,23 +12,19 @@ with io.open('README.rst',  encoding='utf-8') as readme:
 
 setup(
     name='edx-django-sites-extensions',
-    version='2.4.3',
+    version='2.5.0',
     description='Custom extensions for the Django sites framework',
     long_description=long_description,
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet',
         'Intended Audience :: Developers',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
     ],
     keywords='Django sites edx',
@@ -38,7 +34,6 @@ setup(
     license='AGPL',
     packages=['django_sites_extensions'],
     install_requires=[
-        'django>=1.8,<2.0;python_version<"3"',
-        'django>=1.11;python_version>"3"',
+        'django>=2.2,<2.3',
     ],
 )
