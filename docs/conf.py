01718d2ab5450ce0a6804353e8756248d0c326b1
M. Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2020-06-29 16:03:16 +0500

    Run 2to3 (#26)

--- docs/conf.py
+++ docs/conf.py
@@ -48,8 +48,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'edx-django-sites-extensions'
-copyright = u'2016, edX'
+project = 'edx-django-sites-extensions'
+copyright = '2016, edX'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -191,8 +191,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ('index', 'edx-django-sites-extensions.tex', u'edx-django-sites-extensions Documentation',
-     u'edX', 'manual'),
+    ('index', 'edx-django-sites-extensions.tex', 'edx-django-sites-extensions Documentation',
+     'edX', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -221,8 +221,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'edx-django-sites-extensions', u'edx-django-sites-extensions Documentation',
-     [u'edX'], 1)
+    ('index', 'edx-django-sites-extensions', 'edx-django-sites-extensions Documentation',
+     ['edX'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -235,8 +235,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', 'edx-django-sites-extensions', u'edx-django-sites-extensions Documentation',
-     u'edX', 'edx-django-sites-extensions', 'edx-django-sites-extensions',
+    ('index', 'edx-django-sites-extensions', 'edx-django-sites-extensions Documentation',
+     'edX', 'edx-django-sites-extensions', 'edx-django-sites-extensions',
      'Miscellaneous'
      ),
 ]
